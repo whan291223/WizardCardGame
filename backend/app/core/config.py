e25aa007570398081df09073d9c5421dce7dfd05
PAKAPON CHAIMONGKOLTRAKUL <pakapon.chaimongkoltrakul@siemens.com>
2026-05-02 07:52:47 +0700

    remove backend

--- backend/app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:password@localhost:5432/wizard_game"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
